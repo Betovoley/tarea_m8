1e066297306c506d1b680e87bcb14ae3ebdd4c89
Betovoley <beto_voley@hotmail.com>
2024-11-10 16:22:36 -0300

    Trabajo con graficos, instalación de plotly

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import plotly.express as px
 
 
 img = Image.open('Imagenes/logo_sada.png')
@@ -14,6 +15,13 @@ st.set_page_config('MPAD M8',
 def main():
     st.title("Personalizando mi aplicación")
     st.sidebar.header('Mi menu')
+
+    df = pd.read_csv('data/sada_minas.csv')
+    st.dataframe(df)
+
+    df_count = df.groupby('player_role').count().reset_index()
+    fig = px.pie(df_count, values='team', title='Roles por equipo')
+    st.plotly_chart(fig)
 
         
 
