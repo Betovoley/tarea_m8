4cc3e6134140aa82c98830be6cf2d1ea31a0c8a2
Betovoley <beto_voley@hotmail.com>
2024-11-10 16:12:08 -0300

    Personalizando configuraciones sidebar

--- app.py
+++ app.py
@@ -3,26 +3,19 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
+
+img = Image.open('Imagenes/logo_sada.png')
+st.set_page_config('MPAD M8', 
+                    page_icon='img', 
+                    layout='wide',
+                    initial_sidebar_state='collapsed'
+                    )
+
 def main():
-    st.title("Trabajando con controles de ingreso de datos")
+    st.title("Personalizando mi aplicación")
+    st.sidebar.header('Mi menu')
 
-    nombre = st.text_input('Ingrese su nombre')
-    st.write(nombre)
-
-    mensaje = st.text_area('Ingrese su mensaje', height=100)
-    st.write(mensaje)
-
-    edad = st.number_input('Ingrese su edad', 1,100, step=1)
-    st.write(edad)
-
-    fecha = st.date_input('Ingrese su fecha de nacimiento')
-    st.write(fecha)
-
-    hora = st.time_input('Ingrese la hora')
-    st.write(hora)
-
-    color = st.color_picker('seleccione un color')
-    st.write(color)
+        
 
 if __name__ == "__main__":
     main()
