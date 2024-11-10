116604f43435a8cd0c85bb152d32b077be0eef36
Betovoley <beto_voley@hotmail.com>
2024-11-10 15:36:50 -0300

    Trabajando con multimedia

--- app.py
+++ app.py
@@ -3,67 +3,29 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
-df = pd.read_csv('data/sada_minas.csv')
-
 def main():
-    st.title("Trabajando con componentes")
+    st.title("Trabajando con multimedia")
 
-    posicion = st.selectbox('Seleccione una posición', 
-                  ['Portero', 'Defensa', 'Mediocampista', 'Delantero']
-                 )
-    st.write(f"La posición seleccionada es: {posicion}")
+    img = Image.open("imagenes/camiseta.png")
+    st.image(img, use_container_width=True)
 
-    opciones = st.multiselect('Seleccione una posición', 
-                  ['Portero', 'Defensa', 'Mediocampista', 'Delantero']
-                 )
-    st.write(f"La posición seleccionada es: {opciones}")
+    img1="https://upload.wikimedia.org/wikipedia/pt/6/67/SadaCruzeiro2021.png"
+    st.image(img1, use_container_width=True)
 
-    edad = st.slider('Selecciones su edad',
-                min_value = 1,
-                max_value = 100,
-                value = 20,
-                step = 1
-                )
-    st.write(f"Su edad es: {edad}")
+    with open('Videos/yo.mp4', 'rb') as video_file:
+        st.video(video_file, start_time=0)
 
-    nivel = st.select_slider(
-        'Seleccion su nivel',
-        options = ['Muy bajo', 'Bajo', 'Medio', 'Alto', 'Muy alto'],
-        )
-    st.write(f"Su nivel es: {nivel}")
+    with open('Musica/we are the champions.mp3', 'rb') as audio_file:
+        st.audio(audio_file, start_time=0)
 
-    opc = st.radio('selccione una opcion',
-            ['Portero', 'Defensa', 'Mediocampista', 'Delantero']
-            )
-
-    st.write(f"La opcion seleccionada es: {opc}")
-
-    check = st.checkbox("Acepto las condiciones")
-    if check:
-        st.write("Aceptaste las condiciones")
-    else:
-        st.write("No Aceptaste las condiciones")
-
-    rta = st.button('Hola amor')
-    if rta:
-        st.write('Como estas amor?')
-        st.write(rta)
-    else:
-        st.write('-')
-        st.write(rta)
+    st.html(
+    "<p><span style='text-decoration: line-through double red;'>Esta funcionando</span>!</p>"
 
     
-    tab1, tab2, tab3 = st.tabs(["Ataque", "Saque", "Bloqueo"])
+)
 
-    with tab1:
-        st.header("Ataque")
-        st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-    with tab2:
-        st.header("Saque")
-        st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-    with tab3:
-        st.header("Bloqueo")
-        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+    
 
 
 if __name__ == "__main__":
