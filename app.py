8d279a1dffd7854609a85a0a0968c1e8715802a9
Betovoley <beto_voley@hotmail.com>
2024-11-10 14:26:29 -0300

    trabajando con tablas

--- app.py
+++ app.py
@@ -3,32 +3,18 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 
+df = pd.read_csv('data/sada_minas.csv')
+
 def main():
-    st.title("Mi primera app en Streamlit")
-    st.header("este es el header")
-    st.subheader("este es el subheader")
-    st.text("esto es un texto")
-    nombre = "Betovoley"
-    st.text(f"hola {nombre}")
-    nombre = "mundo"
-    st.text(f"hola " + nombre)
-    st.markdown("# markdown 1")
-    st.markdown("## markdown 2")
-    st.markdown("### markdown 3")
-    st.markdown("#### markdown 4")
-    st.markdown("markdown 0")
+    st.title("Trabajando con datos en Streamlit")
+    st.header("Usando dataframe")
+    st.dataframe(df)
+    st.header("Usando table")
+    st.table(df)
 
-    st.success("mensaje de exito")
-    st.error("mensaje de error")
-    st.warning("mensaje de advertencia")
-    st.info("informacion")
-    st.exception("mensaje contro excepciones")
-    st.text("esto es un texto")
-
-    st.write("parrafo o texto que queramos")
-    st.write("### parrafo o texto que queramos")
-    st.write(3 + 3)
-
+    st.header("Visualizando json")
+    st.json("clave:valor")
+    
 
 if __name__ == "__main__":
     main()
